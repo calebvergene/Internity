7cfb8caae183f4234f587292c5394cf24257117d
caleb vergene <calebverg@gmail.com>
2024-07-13 14:45:37 -0700

    google log in works from home page and log out sends back to login page

--- client/app/components/ApplicationList.tsx
+++ client/app/components/ApplicationList.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { useState } from "react";
-import { Dropdown } from "flowbite-react";
 import CustomDropdown from "./CustomDropdown";
 
 interface Application {
@@ -102,17 +101,19 @@ const ApplicationList: React.FC<ApplicationListProps> = ({ applications, updateA
                                         </tr>
                                     )}
                                     <tr className="h-full">
-                                        <div className="flex items-center justify-center my-1">
-                                            <a
-                                                href={application.link}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
+                                        <a
+                                            href={application.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
                                             >
+                                            <td className="flex items-center justify-center font-rubik font-medium">
+                                            
                                                 <button className="">
                                                 {application.name}
                                                 </button>
-                                            </a>
-                                        </div>
+                                            
+                                            </td>
+                                        </a>
                                         <td className="py-2 text-center">
                                         <CustomDropdown
                                             label={<><span className={getStatusClassName(application.status)}> ● </span><span>{application.status}</span></>}
@@ -128,12 +129,12 @@ const ApplicationList: React.FC<ApplicationListProps> = ({ applications, updateA
                                         <td className="py-2 text-center text-sm">{application.open}</td>
                                         <td className="py-2 text-center text-sm">{application.close}</td>
                                         <td className="py-2 text-center flex justify-center text-sm">
-                                            <button onClick={() => onDelete(application.id)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl">
+                                            <button onClick={() => onDelete(application.id)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl mt-1">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9L14.394 18M9.26 18L9.606 9M18.628 5.79C18.97 5.842 19.31 5.897 19.65 5.956M18.628 5.79L18.16 19.673A2.25 2.25 0 0115.916 21H8.084A2.25 2.25 0 015.84 19.673L5.372 5.79M18.628 5.79C17.48 5.618 16.313 5.492 15.13 5.414M4.372 5.79C4.034 5.731 3.694 5.676 3.354 5.617M4.372 5.79A48.108 48.108 0 007.85 5.393M14.5 4.5V3.75C14.5 2.57 13.59 1.586 12.41 1.55A51.964 51.964 0 009.09 1.55C7.91 1.586 7 2.57 7 3.75V4.5M14.5 4.5H9.5M14.5 4.5H9.5M14.5 4.5V5.393M9.5 4.5V5.393" />
                                                 </svg>
                                             </button>
-                                            <button onClick={() => updateApplication(application)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl ">
+                                            <button onClick={() => updateApplication(application)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl mt-1">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                 </svg>
---
+++ client/app/home/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+import { useState, useEffect } from "react";
+import ApplicationList from "../components/ApplicationList"
+import ApplicationForm from "../components/ApplicationForm";
+
+export default function Home() {
+  const [userName, setUserName] = useState('');
+  const [applications, setApplications] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentApplication, setCurrentApplication] = useState({})
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [waterDroplets, setWaterDroplets] = useState(0);
+
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+    try {
+      const response = await fetch("http://localhost:5001/application", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setApplications(data.applications);
+        setUserName(data.userName);
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    } catch (error) {
+      console.error("Error checking login status:", error);
+      setIsLoggedIn(false);
+    }
+  };
+
+
+  // this is an async function, so it has to wait to get the applications
+  const fetchApplications = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // fetch sends a request to the backend API. we are waiting to get
+    // a response, then we get json data
+    const response = await fetch("http://localhost:5001/application")
+    const data = await response.json() 
+    setApplications(data.applications)
+  }
+
+  const handleLogin = async () => {
+    window.location.href = "http://localhost:5001/login";
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("http://localhost:5001/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (response.ok) {
+        setIsLoggedIn(false);
+        setApplications([]);
+      } else {
+        console.error("Error logging out:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentApplication({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (application: any) => {
+    if (isModalOpen) return
+    setCurrentApplication(application)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchApplications
+    location.reload()
+  }
+
+
+  return (
+    <>
+      <div className="flex flex-col items-center mt-3 justify-center">
+        {!isLoggedIn ? ( // if not logged in, i think it needs to have link to /home
+
+          <button
+            onClick={handleLogin}
+            className="bg-blue-500/10 border-transparent font-rubik rounded-md mt-2 py-2 px-3 text-blue-600 flex justify-center place-content-center hover:bg-blue-500/20 hover:text-blue-600 duration-300"
+          >
+            Login with Google
+          </button>
+        ) : ( // displays create app button if logged in
+          <>
+            <div className="flex justify-between items-center w-full px-8">
+              <a href="/">
+              <button className="p-3 rounded-lg px-[15px] hover:border-white hover:bg-gray-300/50 duration-300">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+              </button>
+              </a>
+              <div className="flex items-center">
+                <button className="border-r border-gray-400/50 px-6 py-0">
+                  💧{waterDroplets}
+                </button>
+                <a href="/">
+                  <button
+                    onClick={handleLogout}
+                    className="bg-black border-transparent font-rubik rounded-md my-2 py-2 px-3 ml-6 text-white flex justify-center place-content-center hover:bg-gray-100 hover:text-black duration-300"
+                  >
+                    Logout
+                  </button>
+                </a>
+              </div>
+            </div>
+            <div className='flex flex-grow justify-center'>
+                <h3 className='text-center font-bold'>Glad to have you, {userName}!</h3>
+              </div>
+            <div className="mt-4">
+              <button
+                onClick={openCreateModal}
+                className="bg-gradient-to-r from-green-400 to-blue-400 hover:from-blue-500 hover:to-green-400 duration-300 font-rubik rounded-md mt-2 py-2 px-3 text-white flex justify-center place-content-center">
+                  Create New Application <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 pl-1 pb-[2px] "><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+      <ApplicationList applications={applications} updateApplication={openEditModal} updateCallback={onUpdate}/>
+      {isModalOpen && <div className="position: fixed z-1 left-0 top-0 w-full h-full overflow-auto">
+        <div className="bg-white m-4 p-4 w-4/5">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ApplicationForm existingApplication={currentApplication} updateCallback={onUpdate} closeModal={closeModal}/>
+        </div>
+      </div>
+      }
+    </>
+  );
+  }
+  
