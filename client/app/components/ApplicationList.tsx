614259f86acc7ea8a22b522251b613763e60574a
caleb vergene <calebverg@gmail.com>
2024-08-12 16:58:25 -0700

    added apply button, redirects to link

--- client/app/components/ApplicationList.tsx
+++ client/app/components/ApplicationList.tsx
@@ -95,27 +95,17 @@ const ApplicationList: React.FC<ApplicationListProps> = ({ applications, updateA
                             {applications.map((application, index) => (
                                 <React.Fragment key={application.id}>
                                     {index > 0 && (
-                                        <tr>
+                                        <tr className="">
                                             <td colSpan={5}>
                                                 <hr className="border-t border-gray-300" />
                                             </td>
                                         </tr>
                                     )}
                                     <tr className="h-full">
-                                        <a
-                                            href={application.link}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            >
-                                            <td className="flex items-center justify-center font-rubik font-medium">
-                                            
-                                                <button className="">
-                                                    {application.name}
-                                                </button>
-                                            
-                                            </td>
-                                        </a>
-                                        <td className="py-2 text-center">
+                                        <td className="flex items-center justify-center font-rubik font-medium ">
+                                                {application.name}
+                                        </td>
+                                        <td className=" text-center">
                                         <CustomDropdown
                                             label={<><span className={getStatusClassName(application.status)}> ● </span><span>{application.status}</span></>}
                                             items={[
@@ -130,14 +120,24 @@ const ApplicationList: React.FC<ApplicationListProps> = ({ applications, updateA
                                         <td className="py-2 text-center text-m">{application.open}</td>
                                         <td className="py-2 text-center text-sm">{application.close}</td>
                                         <td className="py-2 text-center flex justify-center text-sm">
-                                            <button onClick={() => onDelete(application.id)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl mt-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9L14.394 18M9.26 18L9.606 9M18.628 5.79C18.97 5.842 19.31 5.897 19.65 5.956M18.628 5.79L18.16 19.673A2.25 2.25 0 0115.916 21H8.084A2.25 2.25 0 015.84 19.673L5.372 5.79M18.628 5.79C17.48 5.618 16.313 5.492 15.13 5.414M4.372 5.79C4.034 5.731 3.694 5.676 3.354 5.617M4.372 5.79A48.108 48.108 0 007.85 5.393M14.5 4.5V3.75C14.5 2.57 13.59 1.586 12.41 1.55A51.964 51.964 0 009.09 1.55C7.91 1.586 7 2.57 7 3.75V4.5M14.5 4.5H9.5M14.5 4.5H9.5M14.5 4.5V5.393M9.5 4.5V5.393" />
-                                                </svg>
+                                        <a
+                                            href={application.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            >
+                                            <button
+                                                className="justify-start bg-green-500 duration-300 font-rubik rounded-md mt-2 py-1 mb-1 mr-2 px-3 text-white flex  place-content-center">
+                                                Apply
                                             </button>
-                                            <button onClick={() => updateApplication(application)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 p-1 rounded-xl mt-1">
+                                        </a>
+                                            <button onClick={() => updateApplication(application)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 py-1 pt-1.5 px-1 rounded-xl mt-1">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                </svg>
+                                            </button>
+                                            <button onClick={() => onDelete(application.id)} className="text-gray-500 flex justify-center place-content-center hover:bg-gray-500/20 hover:text-gray-600 duration-300 py-1 pt-1.5 px-1 rounded-xl mt-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9L14.394 18M9.26 18L9.606 9M18.628 5.79C18.97 5.842 19.31 5.897 19.65 5.956M18.628 5.79L18.16 19.673A2.25 2.25 0 0115.916 21H8.084A2.25 2.25 0 015.84 19.673L5.372 5.79M18.628 5.79C17.48 5.618 16.313 5.492 15.13 5.414M4.372 5.79C4.034 5.731 3.694 5.676 3.354 5.617M4.372 5.79A48.108 48.108 0 007.85 5.393M14.5 4.5V3.75C14.5 2.57 13.59 1.586 12.41 1.55A51.964 51.964 0 009.09 1.55C7.91 1.586 7 2.57 7 3.75V4.5M14.5 4.5H9.5M14.5 4.5H9.5M14.5 4.5V5.393M9.5 4.5V5.393" />
                                                 </svg>
                                             </button>
                                         </td>
