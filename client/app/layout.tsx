4c0faf7e9b74da5bcd2fde766611d0f164e7fcf9
caleb vergene <calebverg@gmail.com>
2024-08-23 11:53:02 -0700

    fixed application database bug

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Applyd",
+  title: "Internity",
 };
 
 export default function RootLayout({
